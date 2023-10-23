7ae75deb4a7b108fb11a25607d02ba2d69143246
benaissa-saida <benaissasaida28@gmail.com>
2023-10-23 21:36:51 +0200

    fix sorting and search

--- src/lib/dataTable.jsx
+++ src/lib/dataTable.jsx
@@ -4,6 +4,7 @@ import Pagination from "./components/pagination";
 import Search from "./components/search";
 import "./default.css";
 import Entries from "./components/entries";
+import isValidDate from "date-fns/isValid";
 
 const DataTable = ({ data, headers }) => {
   const [employeesList, setEmployeesList] = useState([]);
@@ -13,48 +14,60 @@ const DataTable = ({ data, headers }) => {
   const [endIndex, setEndIndex] = useState(0);
   const [search, setSearch] = useState("");
   const [entries, setEntries] = useState(10);
-  const [sorting, setSorting] = useState({ field: "firstName", order: "asc" });
+  const [firstField, setFirstField] = useState("");
+  const [sorting, setSorting] = useState({ field: firstField, order: "asc" });
 
   useEffect(() => {
     setEmployeesList(data);
-  }, [data]);
+    if (data.length > 0) {
+      setFirstField(Object.keys(data[0])[0]);
+    }
+  }, [data, setFirstField]);
 
   const employeesListData = useMemo(() => {
+    console.log(employeesList);
     let computedEmployeesList = employeesList;
 
     if (search) {
-      computedEmployeesList = computedEmployeesList.filter(
-        (employee) =>
-          employee.firstName.toLowerCase().includes(search.toLowerCase()) ||
-          employee.lastName.toLowerCase().includes(search.toLowerCase()) ||
-          employee.dateBirth.includes(search.toLowerCase()) ||
-          employee.startDate.includes(search.toLowerCase()) ||
-          employee.department.toLowerCase().includes(search.toLowerCase()) ||
-          employee.street.toLowerCase().includes(search.toLowerCase()) ||
-          employee.city.toLowerCase().includes(search.toLowerCase()) ||
-          employee.state.toLowerCase().includes(search.toLowerCase()) ||
-          employee.zip.includes(search.toLowerCase())
-      );
+      computedEmployeesList = computedEmployeesList.filter((obj) => {
+        //filter if search includes value of field tolowercase
+        //else filter if search includes value of field
+        for (const key in obj) {
+          if (
+            typeof obj[key] === "string" &&
+            obj[key].toLowerCase().includes(search.toLowerCase())
+          ) {
+            return true;
+          } else if (obj[key].includes(search)) {
+            return true;
+          }
+        }
+        return false;
+      });
       console.log(computedEmployeesList);
     }
 
     setTotalItems(computedEmployeesList.length);
 
     const reversed = sorting.order === "asc" ? 1 : -1;
-    if (sorting.field === "dateOfBirth" || sorting.field === "startDate") {
-      //Sorting dates
-      computedEmployeesList = computedEmployeesList.sort(
-        (a, b) =>
+
+    //Sorting employeesList
+    computedEmployeesList = computedEmployeesList.sort((a, b) => {
+      if (
+        isValidDate(new Date(a[sorting.field])) ||
+        isValidDate(new Date(b[sorting.field]))
+      ) {
+        return (
           reversed *
           (new Date(a[sorting.field]).getTime() -
             new Date(b[sorting.field]).getTime())
-      );
-    } else {
-      //Sorting employeesList
-      computedEmployeesList = computedEmployeesList.sort(
-        (a, b) => reversed * a[sorting.field].localeCompare(b[sorting.field])
-      );
-    }
+        );
+      } else if (sorting.field !== "") {
+        return reversed * a[sorting.field].localeCompare(b[sorting.field]);
+      } else {
+        return [];
+      }
+    });
     setStartIndex((currentPage - 1) * entries);
     setEndIndex(startIndex + entries);
 
